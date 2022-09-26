2e35d96dec452b46c3f3ccd891a7da2873ed9e94
Rava-milad <devskil2266@gmail.com>
2022-09-27 00:59:16 +0330

    rtl the theme for blog structure

---
+++ resources/views/components/post-featured-card.blade.php
@@ -0,0 +1,67 @@
+<article
+            class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
+            <div class="py-6 px-5 lg:flex">
+                <div class="flex-1 lg:ml-8">
+                    <img src="{{ asset('images/illustration-1.png') }}" alt="Blog Post illustration" class="rounded-xl">
+                </div>
+
+                <div class="flex-1 flex flex-col justify-between">
+                    <header class="mt-8 lg:mt-0">
+                        <div class="space-x-2">
+                            <a href="#"
+                               class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
+                               style="font-size: 10px">تکنیک ها</a>
+
+                            <a href="#"
+                               class="px-3 py-1 border border-red-300 rounded-full text-red-300 text-xs uppercase font-semibold"
+                               style="font-size: 10px">آپدیت ها</a>
+                        </div>
+
+                        <div class="mt-4">
+                            <h1 class="text-3xl">
+                                این یک عنوان بزرگ برای نمایش است که اگر تا دو یا سه خط آنرا تایپ کنید زیبا تر میشود,
+                                واوو!
+                            </h1>
+
+                            <span class="mt-2 block text-gray-400 text-xs">
+                                        تاریخ انتشار <time>1 روز پیش</time>
+                                    </span>
+                        </div>
+                    </header>
+
+                    <div class="text-sm mt-2">
+                        <p>
+                            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک
+                            است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی
+                            تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی
+                            در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم
+                            افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان
+                            فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و
+                            شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات
+                            پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+                        </p>
+
+                        <p class="mt-4">
+                            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است
+                            چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است.
+                        </p>
+                    </div>
+
+                    <footer class="flex justify-between items-center mt-8">
+                        <div class="flex items-center text-sm">
+                            <img src="{{ asset('images/lary-avatar.svg') }}" alt="Lary avatar">
+                            <div class="mx-3">
+                                <h5 class="font-bold">میلاد رحیمی</h5>
+                                <h6>برنامه نویس جونیور</h6>
+                            </div>
+                        </div>
+
+                        <div class="hidden lg:block">
+                            <a href="#"
+                               class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8"
+                            >مطالعه بیشتر</a>
+                        </div>
+                    </footer>
+                </div>
+            </div>
+        </article>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,8 +14,7 @@
         </article>
     @endforeach
 @endsection --}}
-<x-layout>
-    @foreach ($posts as $post)
+    {{--@foreach ($posts as $post)
         <article class="{{ $loop->even ? 'foo' : '' }}">
             <a href="/posts/{{ $post->slug }}">
                 <h1>{!! $post->title !!}</h1>
@@ -26,5 +25,20 @@
                 {!! $post->excerpt !!}
             </div>
         </article>
-    @endforeach
+    @endforeach--}}
+<x-layout>
+    @include('_post-header')
+
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+            <x-post-featured-card />
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card />
+            <x-post-card />
+        </div>
+        <div class="lg:grid lg:grid-cols-3">
+            <x-post-card />
+            <x-post-card />
+            <x-post-card />
+        </div>
+    </main>
 </x-layout>
